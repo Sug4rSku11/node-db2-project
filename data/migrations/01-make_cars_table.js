1b3f469e7d6ef43f97a365a473feeff7a4ab53c8
Desiree Garcia <57469534+Sug4rSku11@users.noreply.github.com>
2021-12-07 18:29:39 -0700

    created database, was able to add cars

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,22 @@
-exports.up = function (knex) {
-  // DO YOUR MAGIC
+exports.up = async function (knex) {
+  await knex.schema.createTable('cars', table => {
+    table.increments('id')
+    table.text('vin')
+    .notNullable()
+    .unique()
+    table.text('make')
+    .notNullable()
+    table.text('model')
+    .notNullable()
+    table.decimal('milage')
+    .notNullable()
+    table.text('title')
+    .defaultTo(false)
+    table.text('transmission')
+    .defaultTo(false)
+  })
 };
 
-exports.down = function (knex) {
-  // DO YOUR MAGIC
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists('cars')
 };
